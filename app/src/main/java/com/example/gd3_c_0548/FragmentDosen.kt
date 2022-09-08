2cbad0e4001746a3fa78a4361bf3ec580fa04851
philipferdinand <philipfs150302@gmail.com>
2022-09-08 13:30:32 +0700

    Initial commit

---
+++ app/src/main/java/com/example/gd3_c_0548/FragmentDosen.kt
@@ -0,0 +1,31 @@
+package com.example.gd3_c_0548
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import entity.Dosen
+
+class FragmentDosen : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_dosen, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val layoutManager = LinearLayoutManager(context)
+        val adapter : RVDosenAdapter = RVDosenAdapter(Dosen.listOfDosen)
+        val rvDosen : RecyclerView = view.findViewById(R.id.rv_dosen)
+        rvDosen.layoutManager = layoutManager
+        rvDosen.setHasFixedSize(true)
+        rvDosen.adapter = adapter
+    }
+
+}
---
+++ app/src/main/java/entity/Dosen.kt
@@ -0,0 +1,12 @@
+package entity
+
+class Dosen(var name: String, var pengajar: String) {
+
+    companion object{
+        @JvmField
+        var listOfDosen = arrayOf(
+            Dosen("Fedelis Brian", "Pengajar Kelas A, B, dan D"),
+            Dosen("Thomas Adi", "Pengajar Kelas C"),
+        )
+    }
+}
